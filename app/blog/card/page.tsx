ca2f4f2a7083b244036327166ca9ae3059ba3c21
安红豆 <ormissia@outlook.com>
2025-10-19 00:00:28 +0800

    :tada:

--- app/blog/card/page.tsx
+++ app/blog/card/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import "./page.css"
 
 // https://codepen.io/rabakilgur/pen/RYpVQz
@@ -7,7 +8,7 @@ export default function BlogCard() {
         <div className="projcard projcard-blue">
             <Link href="/blog/123">
                 <div className="projcard-innerbox">
-                    <img className="projcard-img" src="https://picsum.photos/800/600?image=1041" alt="Blog card preview image"/>
+                    <Image className="projcard-img" src="https://picsum.photos/800/600?image=1041" alt="Blog card preview image" width={800} height={600} priority/>
                     <div className="projcard-textbox">
                         <div className="projcard-title">Card Title</div>
                         <div className="projcard-subtitle">This explains the card in more detail</div>
---
+++ app/blog/reading-notes/page.tsx
@@ -0,0 +1,8 @@
+export default function ReadingNotes() {
+  return (
+    <div className="reading-notes-container">
+      <h1>Reading Notes</h1>
+      <p>Reading notes page content goes here.</p>
+    </div>
+  );
+}
